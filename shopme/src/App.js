21818b6c1084dff2e30e7d4cd647edf08c462338
Ziarnox <norbertziarno@gmail.com>
2024-03-22 21:02:37 +0100

    Basics for react and simple Navi

---
+++ shopme/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Nav from "./navigation/Nav";
+import Products from './products/Products';
+import Recomended from './recomended/Recomended';
+
+function App() {
+  return (
+  <>
+      <Nav/>
+      <Products/>
+      <Recomended/>
+  </>
+
+  );
+}
+
+export default App;
---
+++ shopme/src/recomended/Recomended.js
@@ -0,0 +1,9 @@
+import './Recomended.css';
+
+function Recomended() {
+  return (
+    <div>Recomended</div>
+  )
+}
+
+export default Recomended;
